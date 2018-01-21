a0e772d0954f3933f2712d2015c54f7d473893d3
mo.programmer <mo.programmer@hotmail.com>
2018-01-21 13:23:35 +0100

    sum func

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,5 +12,14 @@ namespace ConsoleApp1
         {
             // changes 2
         }
+
+        public int sum()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+
+            return sum;
+        }
     }
 }
